0ca5f714d7048808d0232ec4adebce687a8a1019
Dandada1993 <35353137+Dandada1993@users.noreply.github.com>
2022-02-02 07:08:21 -0400

    Update snippets.lua

--- snippets.lua
+++ snippets.lua
@@ -281,29 +281,3 @@ Debris:AddItem(projectile, 3)
 -- Make sure the part is anchored, add a script with the following
 local part = script.Parent
 part.Velocity = Vector3.new(20, 0, 0) -- gives a velocity in the X direction
-
--- SHIFT TO SPEED WALK
--- Add this to StarterPlayer->StarterPlayerScripts
-
-local UserInputService = game:GetService("UserInputService")
-local player = game:GetService("Players").LocalPlayer
-local character = player.Character or player.CharacterAdded:Wait()
-local humanoid = character:FindFirstChildWhichIsA("Humaonoid")
-
-local FAST_WALK = 32
-local NORMAL_WALK = 16
-
-local function OnInputBegan(input, gameProcessed)
-  if input.KeyCode == Enum.KeyCode.LeftShift or input.KeyCode == Enum.KeyCode.RightShift then
-    humanoid.WalkSpeed = FAST_WALK -- character.Humanoid.WalkSpeed = FAST_WALK
-  end
-end
-
-local function OnInputEnded(input, gameProcessed)
-  if input.KeyCode == Enum.KeyCode.LeftShift or input.KeyCode == Enum.KeyCode.RightShift then
-    humanoid.WalkSpeed = NORMAL_WALK -- character.Humanoid.WalkSpeed = NORMAL_WALK
-  end
-end
-
-UserInputService.InputBegan:Connect(OnInputBegan)
-UserInputService.InputEnded:Connect(OnInputEnded)
