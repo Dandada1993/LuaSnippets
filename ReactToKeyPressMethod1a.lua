a68d79ea5f9b6411b43b493ee3e432f362df469f
Dandada1993 <35353137+Dandada1993@users.noreply.github.com>
2022-02-02 07:09:08 -0400

    Create ReactToKeyPressMethod1a.lua

---
+++ ReactToKeyPressMethod1a.lua
@@ -0,0 +1,25 @@
+-- SHIFT TO SPEED WALK
+-- Add this to StarterPlayer->StarterPlayerScripts
+
+local UserInputService = game:GetService("UserInputService")
+local player = game:GetService("Players").LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
+local humanoid = character:FindFirstChildWhichIsA("Humaonoid")
+
+local FAST_WALK = 32
+local NORMAL_WALK = 16
+
+local function OnInputBegan(input, gameProcessed)
+  if input.KeyCode == Enum.KeyCode.LeftShift or input.KeyCode == Enum.KeyCode.RightShift then
+    humanoid.WalkSpeed = FAST_WALK -- character.Humanoid.WalkSpeed = FAST_WALK
+  end
+end
+
+local function OnInputEnded(input, gameProcessed)
+  if input.KeyCode == Enum.KeyCode.LeftShift or input.KeyCode == Enum.KeyCode.RightShift then
+    humanoid.WalkSpeed = NORMAL_WALK -- character.Humanoid.WalkSpeed = NORMAL_WALK
+  end
+end
+
+UserInputService.InputBegan:Connect(OnInputBegan)
+UserInputService.InputEnded:Connect(OnInputEnded)
