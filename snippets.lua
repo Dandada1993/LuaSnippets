4837c2a99ed8a6cbd4940cd47a4918e42a9e1879
Dandada1993 <35353137+Dandada1993@users.noreply.github.com>
2022-04-20 12:59:41 -0400

    ContextActionService bound on tool.Equipped

--- snippets.lua
+++ snippets.lua
@@ -339,3 +339,24 @@ kickAnimationTrack:Play()
 kickAnimationTrack:GetMarkerReachedSignal("KickEnd"):Connect(function(paramString)
 	print(paramString)
 end)
+
+-- Bind Action when tool equiped. LocalScript parented to the tool
+local ContextActionService = game:GetService("ContextActionService")
+ 
+local ACTION_RELOAD = "Reload"
+ 
+local tool = script.Parent
+ 
+local function handleAction(actionName, inputState, inputObject)
+	if actionName == ACTION_RELOAD and inputState == Enum.UserInputState.Begin then
+		print("Reloading!")
+	end
+end
+ 
+tool.Equipped:Connect(function ()
+	ContextActionService:BindAction(ACTION_RELOAD, handleAction, true, Enum.KeyCode.R)
+end)
+ 
+tool.Unequipped:Connect(function ()
+	ContextActionService:UnbindAction(ACTION_RELOAD)
+end)
